3f90aab023c3f81854385377ef59a90db5bf4234
gopalatejaswi <gopalatejaswi@gmail.com>
2026-02-26 21:12:12 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "../components/theme-provider";
 import Header from "../components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,15 @@
-import { PrismaClient } from '../app/generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"], // optional
+  });
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
 
 
